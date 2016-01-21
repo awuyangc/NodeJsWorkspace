36499c3dfe3f7107ea5afecdb81732772582ef0c
wuyang <289677561@qq.com>
2016-01-21 12:25:19 +0800

    20160121

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -47,87 +47,83 @@ function checkForm(){
     }
 }
 //添加列表
-function addItems(number, lastIndex) {
+function addItems(title,src,content) {
     // 生成新条目的HTML
-    var html = '';
-    for (var i = lastIndex + 1; i <= lastIndex + number; i++) {
-       /* html += '<li class="item-content">' +
-                    '<div class="card" style="width:80%">' +
-                        '<div class="card-header">新的公布:</div>' +
+   var html = '<li class="item-content">' +
+                    '<div class="card" style="width:90%">' +
+                        '<div class="card-header">'+title+'</div>' +
                         '<div class="card-content">' +
                         '<div class="list-block media-list">' +
                             '<ul style="padding-left: 0rem;">' +
                                 '<li class="item-content">' +
                                     '<div class="item-media">' +
-                                        '<img src="http://gqianniu.alicdn.com/bao/uploaded/i4//tfscom/i3/TB10LfcHFXXXXXKXpXXXXXXXXXX_!!0-item_pic.jpg_250x250q60.jpg" width="44">' +
+                                        '<img src="'+src+'" width="60">' +
                                     '</div>' +
                                     '<div class="item-inner">' +
-                                        '<div class="item-title-row">' +
-                                            '<div class="item-title">标题</div>' +
+                                        '<div class="item-content">' +
+                                            content +
                                         '</div>' +
-                                        '<div class="item-subtitle">子标题</div>' +
                                     '</div>' +
                                 '</li>' +
                             '</ul>' +
                         '</div>' +
                         '</div>' +
                     '</div>' +
-                '</li>';
-                */
-        html += '<li class="item-content"><div class="item-inner"><div class="item-title">Item ' + i + '</div></div></li>';
-    }
-
+            '</li>';
     // 添加新条目
     $('.infinite-scroll-bottom .list-container').append(html);
-
 }
 
 //
 $(document).on("pageInit", "#page3", function(e, pageId, $page) {
-    var url="http://yuntuapi.amap.com/datasearch/local?tableid=55656259e4b0ccb608f13383&city=武汉市&keywords=&limit=10&page=1&key=861347745e27c5cc79e4aa86befb961a";
+    // 加载flag
+    var loading = false;
+    // 最多可加载的条目
+    var maxItems = 100;
+    // 每次加载添加多少条目
+    var itemsPerLoad = 10;
+    var url="http://yuntuapi.amap.com/datasearch/local?tableid=55656259e4b0ccb608f13383&city=武汉市&keywords=&limit="+itemsPerLoad+"&page=1&key=861347745e27c5cc79e4aa86befb961a";
     $.ajax({
         dataType : "jsonp",
         url:url,
         success: function (result) {
             $(result.datas).each(function(i,val){
-
+                //初始化
+                addItems(val._name+val._id,val._image.length>0?val._image[0]._preurl:'',val._address);
             });
-            // 加载flag
-            var loading = false;
-            // 最多可加载的条目
-            var maxItems = 100;
-            // 每次加载添加多少条目
-            var itemsPerLoad = 10;
-            var lastIndex = $('.list-container li').length;
-            //如果没有数据则预先加载20条
-            if(lastIndex==0){
-                addItems(itemsPerLoad, 0);
-                //上次加载的序号
-                lastIndex = itemsPerLoad;
-            }
             // 注册'infinite'事件处理函数
             $(document).on('infinite', '.infinite-scroll-bottom',function() {
                 // 如果正在加载，则退出
                 if (loading) return;
                 // 设置flag
                 loading = true;
+                var lastIndex = $('.list-container .card').length;
                 // 模拟1s的加载过程
                 setTimeout(function() {
-                    // 重置加载flag
-                    loading = false;
-                    if (lastIndex >= maxItems) {
-                        // 加载完毕，则注销无限加载事件，以防不必要的加载
-                        $.detachInfiniteScroll($('.infinite-scroll'));
-                        // 删除加载提示符
-                        $('.infinite-scroll-preloader').remove();
-                        return;
-                    }
-                    // 添加新条目
-                    addItems(itemsPerLoad, lastIndex);
-                    // 更新最后加载的序号
-                    lastIndex = $('.list-container li').length;
-                    //容器发生改变,如果是js滚动，需要刷新滚动
-                    $.refreshScroller();
+                    var url="http://yuntuapi.amap.com/datasearch/local?tableid=55656259e4b0ccb608f13383&city=武汉市&keywords=&limit="+itemsPerLoad+"&page="+(lastIndex/itemsPerLoad+1)+"&key=861347745e27c5cc79e4aa86befb961a";
+                    $.ajax({
+                        dataType : "jsonp",
+                        url:url,
+                        success: function (result) {
+                            $(result.datas).each(function(i,val){
+                                //初始化
+                                addItems(val._name+val._id,val._image.length>0?val._image[0]._preurl:'',val._address);
+                            });
+                            // 重置加载flag
+                            loading = false;
+                            //如果获得的结果数量小于默认数量则判断加载完毕
+                            if (result.datas.length<itemsPerLoad) {
+                                // 加载完毕，则注销无限加载事件，以防不必要的加载
+                                $.detachInfiniteScroll($('.infinite-scroll'));
+                                // 删除加载提示符
+                                $('.infinite-scroll-preloader').remove();
+                                return;
+                            }
+                            //容器发生改变,如果是js滚动，需要刷新滚动
+                            $.refreshScroller();
+                        }
+                    })
+
                 }, 500);
             });
         }
